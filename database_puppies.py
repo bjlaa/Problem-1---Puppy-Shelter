b001556f75bbeb2a88493eb21ef05e05bf86bad1
Benjamin <benjamin.j.arias@gmail.com>
2015-12-15 12:57:38 +0100

    feat: Created Shelter and Puppy classes

--- database_puppies.py
+++ database_puppies.py
@@ -6,3 +6,25 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 
 Base = declarative_base()
+
+class Shelter(Base):
+	__tablename__ = 'shelter'
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(250), nullable = False)
+	address = Column(String(250))
+	city = Column(String(250))
+	state = Column(String(250))
+	zipCode = Column(Integer)
+	website = Column(String(250))
+
+class Puppy(Base):
+	__tablename__ = 'puppy'
+
+	id = Column(Integer, primary_key = True)
+	name = Column(String(250), nullable = False)
+	dateOfBirth = Column(Date)
+	gender = Column(String(15))
+	weight = Column(Numeric)
+	shelter_id = Column(Integer, ForeignKey('shelter.id'))
+	shelter = relationship(Shelter)
