ebeb4d2ddf2b449654ea8386e70b7d778a67e76d
Benjamin <benjamin.j.arias@gmail.com>
2015-12-15 13:26:33 +0100

    feat: Imported puppies

--- exercise1.py
+++ exercise1.py
@@ -6,4 +6,7 @@ engine = create_engine('sqlite:///exercise2', echo=True)
 from sqlalchemy.orm import sessionmaker
 Session = sessionmaker(bind=engine)
 
+from puppies import Base, Shelter, Puppy
+
 session = Session()
+
