faf90da79a56ff8d7a3dcd63a1ec07d19717231d
Benjamin <benjamin.j.arias@gmail.com>
2015-12-16 14:21:23 +0100

    feat: Created query 4 and 5

--- exercise1.py
+++ exercise1.py
@@ -12,13 +12,20 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 #1. Query all of the puppies and return the results in ascending alphabetical order
-dogs = session.query(Puppy.name).order_by(Puppy.name).all()
-print dogs
+dogs = session.query(Puppy).order_by(Puppy.name).all()
+dogsNames = session.query(Puppy.name).order_by(Puppy.name).all()
+#print dogs
+#print dogsNames
 
 #2. Query all of the puppies that are less than 6 months old organized by the youngest first
-#youngDogs = session.query(Puppy.dateOfBirth).filter(Puppy.dateOfBirth > 180)
+sixMonthsOld = datetime.date.today() - datetime.timedelta(days=180)
+youngDogs = session.query(Puppy.name).filter(Puppy.dateOfBirth > sixMonthsOld)
 #print youngDogs
 
 #3. Query all puppies by ascending weight
 weightDogs = session.query(Puppy.weight).order_by(Puppy.weight).all()
-print weightDogs
+#print weightDogs
+
+#4. Query all puppies grouped by the shelter in which they are staying
+puppiesGrouped = session.query(Puppy).join(Shelter).order_by(Puppy.shelter_id).all()
+print puppiesGrouped
