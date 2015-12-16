212b02b4e1066e87734c63e097155898436c3a76
Benjamin <benjamin.j.arias@gmail.com>
2015-12-16 11:21:59 +0100

    feat: Created second query(not working)

--- exercise1.py
+++ exercise1.py
@@ -10,5 +10,10 @@ Base.metadata.bind=engine
 Session = sessionmaker(bind=engine)
 session = Session()
 
+#1. Query all of the puppies and return the results in ascending alphabetical order
 dogs = session.query(Puppy.name).order_by(Puppy.name).all()
 print dogs
+
+#2. Query all of the puppies that are less than 6 months old organized by the youngest first
+youngDogs = session.query(Puppy.dateOfBirth).filter(Puppy.dateOfBirth > 180)
+print youngDogs
